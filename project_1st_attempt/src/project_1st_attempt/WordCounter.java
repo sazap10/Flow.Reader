377fbffcae3ea4efe23ec79c870581be3f2822c6
jim <jim@jim-Ferrari-One-200>
2012-12-03 14:29:18 +0000

    created 'Word' class for storing the fontsize and content of each word in the cloud, also added function for printing the n most common words

--- project_1st_attempt/src/project_1st_attempt/WordCounter.java
+++ project_1st_attempt/src/project_1st_attempt/WordCounter.java
@@ -12,15 +12,22 @@ import java.util.LinkedHashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Collections;
+import javafx.scene.text.Text;
+import java.util.Map;
 /**
  *
  * @author jim
  */
 public class WordCounter {
  private HashMap<String, Integer> words;
- 
+ private Integer numOfWordsInCloud = 30;
+ private Integer maxFontSize = 40;
+ private Integer maxCount;
+ private Integer minCount;
+ private ArrayList<Word> wordObjects;
  public WordCounter(){
      this.words = new HashMap<String, Integer>();
+     this.wordObjects = new ArrayList<Word>();
  }
  
  //counts words in a line of texts and adds them to the 'words' hashMap
@@ -46,7 +53,8 @@ public class WordCounter {
  public String getWordCount(){
      String output = "";
      Integer currentMax = 0;
-     for (String i: words.keySet()){
+     return this.getNodes();
+     /*for (String i: words.keySet()){
         if (this.words.get(i) > currentMax){
            output = ("the word:" + i + ":" + "        " + this.words.get(i) + "\n");
            currentMax = this.words.get(i);
@@ -55,7 +63,7 @@ public class WordCounter {
      }
     
      return output;
-     
+ */    
  }
  
  public String trimPunctuation(String word){
@@ -63,5 +71,67 @@ public class WordCounter {
      
  }
  
+ public String getNodes(){
+     String output = "";
+     String maxKey = "";
+     int tmpCount;
+     boolean maxCount= false;
+     boolean minCount = false;
+    for (int i = 0; i <= this.numOfWordsInCloud; i++){
+       Integer maxInMap = Collections.max(this.words.values());
+       Iterator it = this.words.entrySet().iterator();
+        while (it.hasNext()) {
+          
+           Map.Entry pairs = (Map.Entry)it.next();
+           //System.out.println(pairs.getKey() + " = " + pairs.getValue());
+          
+           tmpCount = Integer.parseInt(pairs.getValue().toString());
+           if (tmpCount == maxInMap){
+              output += (pairs.getKey().toString()+ ":" + "        " + tmpCount + "\n");
+              maxKey = pairs.getKey().toString();
+              this.wordObjects.add(new Word(maxKey, tmpCount));
+              if (!(maxCount)){
+                  this.maxCount = tmpCount;
+                  maxCount = true;
+                  }
+              }
+                
+        }
+        this.words.remove(maxKey);
+    this.minCount = maxInMap; 
+    }
+    
+    return output;
+ }
+ 
+ public void setWordFontsizes(){
+     for (Word word : wordObjects){
+         int countDiff = word.getCount() - this.maxCount;
+         int totalCountDiff = this.maxCount - this.minCount;
+         word.setFontSize( ((this.maxFontSize* countDiff) / totalCountDiff));
+         
+     }
+ }
+ 
+ 
+ 
 
 }
+class Word {
+    
+    private String letters;
+    private Integer count;
+    private Integer fontSize;
+    public Word(String letters, Integer count){
+        this.letters = letters;
+        this.count = count;
+    }
+    
+    public void setFontSize(Integer size){
+        this.fontSize = size;
+    }
+    public Integer getCount(){
+        return this.count;
+    }
+     
+ }
