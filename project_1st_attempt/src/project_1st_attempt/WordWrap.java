0ac2d363494583f3e79ca6312b67d07d8701aa4f
Sachin Pande <sazap10@gmail.com>
2012-12-03 07:53:49 +0000

    Added file chooser to WordWrap

--- project_1st_attempt/src/project_1st_attempt/WordWrap.java
+++ project_1st_attempt/src/project_1st_attempt/WordWrap.java
@@ -1,4 +1,6 @@
 package project_1st_attempt;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
@@ -13,75 +15,94 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
- 
 public class WordWrap extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    public ArrayList<Text> readFile(Rectangle bound){
-    	ArrayList<Text> pages = new ArrayList<Text>();
-    	double boundWidth = bound.getWidth();
-    	double boundHeight = bound.getHeight();
-    	double spaceLeft = boundWidth;
-    	double spaceWidth = new Text(" ").getBoundsInLocal().getWidth();
-    	double lineHeight = new Text("").getBoundsInLocal().getWidth();
-    	pages.add( new Text(""));
-    	//File file = new File("./test.txt");
-    	int i = 0;
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public ArrayList<Text> readFile(Rectangle bound,File file) {
+		ArrayList<Text> pages = new ArrayList<Text>();
+		double boundWidth = bound.getWidth();
+		double boundHeight = bound.getHeight();
+		double spaceLeft = boundWidth;
+		double spaceWidth = new Text(" ").getBoundsInLocal().getWidth();
+		double lineHeight = new Text("").getBoundsInLocal().getWidth();
+		pages.add(new Text(""));
+		// File file = new File("./test.txt");
+		int i = 0;
 		try {
-			LineNumberReader r = new LineNumberReader(new FileReader("test.txt"));
+			LineNumberReader r = new LineNumberReader(
+					new FileReader(file));
 			String l, s;
-			Text temp= new Text();
+			Text temp = new Text();
 			while ((l = r.readLine()) != null) {
-				Scanner sc = new Scanner(l);			
-		    	while(sc.hasNext())
-		    	{
-		    		s=sc.next();
-		    		temp.setText(s);
-		    		double wordWidth = temp.getBoundsInLocal().getWidth();
-		    		if(wordWidth+spaceWidth>spaceLeft){
-		    			if(pages.get(i).getBoundsInLocal().getHeight()+lineHeight>boundHeight){
-		    				pages.add(new Text());
-		    				i++;
-		    			}else{
-		    				pages.get(i).setText(pages.get(i).getText()+"\n");
-		    			}
-		    			spaceLeft = boundWidth-wordWidth;
-		    		}else{
-		    			spaceLeft = spaceLeft - (wordWidth + spaceWidth);
-		    		}
-		    		pages.get(i).setText(pages.get(i).getText()+s+ " ");
-		    	}
-			    if(!((pages.get(i).getBoundsInLocal().getHeight()+lineHeight)>boundHeight)){
-			    	pages.get(i).setText(pages.get(i).getText()+"\n");
-			    	spaceLeft = boundWidth;
-		    	}
-		    	sc.close();
-	
+				Scanner sc = new Scanner(l);
+				while (sc.hasNext()) {
+					s = sc.next();
+					temp.setText(s);
+					double wordWidth = temp.getBoundsInLocal().getWidth();
+					if (wordWidth + spaceWidth > spaceLeft) {
+						if (pages.get(i).getBoundsInLocal().getHeight()
+								+ lineHeight > boundHeight) {
+							pages.add(new Text());
+							i++;
+						} else {
+							pages.get(i).setText(pages.get(i).getText() + "\n");
+						}
+						spaceLeft = boundWidth - wordWidth;
+					} else {
+						spaceLeft = spaceLeft - (wordWidth + spaceWidth);
+					}
+					pages.get(i).setText(pages.get(i).getText() + s + " ");
+				}
+				if (!((pages.get(i).getBoundsInLocal().getHeight() + lineHeight) > boundHeight)) {
+					pages.get(i).setText(pages.get(i).getText() + "\n");
+					spaceLeft = boundWidth;
+				}
+				sc.close();
+
 			}
 			r.close();
 		} catch (IOException e) {
-				e.printStackTrace();
+			e.printStackTrace();
 		}
-    	return pages;
-    }
-    @Override
-    public void start(final Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        final Rectangle r = new Rectangle(400,500);
-        final StackPane root = new StackPane();
-        FlowPane flow = new FlowPane();
-        flow.setPadding(new Insets(5, 0, 5, 0));
-        flow.setHgap(4);
-        r.setFill(Color.WHITE);
-        r.setStroke(Color.BLACK);
-        ArrayList<Text> pages = readFile(r);
-        	flow.getChildren().addAll(pages);
-        root.getChildren().add(flow);
-        primaryStage.setScene(new Scene(root, 900, 600));
-        primaryStage.show();
-        
-    }
+		return pages;
+	}
+
+	@Override
+	public void start(final Stage primaryStage) {
+		primaryStage.setTitle("Hello World!");
+		final Rectangle r = new Rectangle(400, 500);
+		final StackPane root = new StackPane();
+		FlowPane flow = new FlowPane();
+		flow.setPadding(new Insets(5, 0, 5, 0));
+		flow.setHgap(4);
+		r.setFill(Color.WHITE);
+		r.setStroke(Color.BLACK);
+		File file = openFile(primaryStage);
+		ArrayList<Text> pages = readFile(r,file);
+		flow.getChildren().addAll(pages);
+		root.getChildren().add(flow);
+		primaryStage.setScene(new Scene(root, 900, 600));
+		primaryStage.show();
+
+	}
+	
+	public File openFile(Stage primaryStage){
+		File f = new File(System.getProperty("user.dir"));
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Please choose a text file to read");
+        fileChooser.setInitialDirectory(f);
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showOpenDialog(primaryStage);
+        return file;
+	}
 }
