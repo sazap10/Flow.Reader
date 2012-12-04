94e889e513b2d981e192d84d3ff34a0684659854
jim <jim@jim-Ferrari-One-200>
2012-12-04 21:00:33 +0000

    git is telling me to commit it

--- project_1st_attempt/src/project_1st_attempt/WordWrap.java
+++ project_1st_attempt/src/project_1st_attempt/WordWrap.java
@@ -1,88 +1,65 @@
 package project_1st_attempt;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
- 
-public class WordWrap extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    public ArrayList<Text> readFile(Rectangle bound){
-    	ArrayList<Text> pages = new ArrayList<Text>();
-    	double boundWidth = bound.getWidth();
-    	double boundHeight = bound.getHeight();
-    	double spaceLeft = boundWidth;
-    	double spaceWidth = new Text(" ").getBoundsInLocal().getWidth();
-    	double lineHeight = new Text("").getBoundsInLocal().getWidth();
-    	pages.add( new Text(""));
-    	//File file = new File("./test.txt");
-    	int i = 0;
-		try {
-			LineNumberReader r = new LineNumberReader(new FileReader("test.txt"));
-			String l, s;
-			Text temp= new Text();
-			while ((l = r.readLine()) != null) {
-				Scanner sc = new Scanner(l);			
-		    	while(sc.hasNext())
-		    	{
-		    		s=sc.next();
-		    		temp.setText(s);
-		    		double wordWidth = temp.getBoundsInLocal().getWidth();
-		    		if(wordWidth+spaceWidth>spaceLeft){
-		    			if(pages.get(i).getBoundsInLocal().getHeight()+lineHeight>boundHeight){
-		    				pages.add(new Text());
-		    				i++;
-		    			}else{
-		    				pages.get(i).setText(pages.get(i).getText()+"\n");
-		    			}
-		    			spaceLeft = boundWidth-wordWidth;
-		    		}else{
-		    			spaceLeft = spaceLeft - (wordWidth + spaceWidth);
-		    		}
-		    		pages.get(i).setText(pages.get(i).getText()+s+ " ");
-		    	}
-			    if(!((pages.get(i).getBoundsInLocal().getHeight()+lineHeight)>boundHeight)){
-			    	pages.get(i).setText(pages.get(i).getText()+"\n");
-			    	spaceLeft = boundWidth;
-		    	}
-		    	sc.close();
-	
+public class WordWrap {
+	ArrayList<String> pages;
+	File file;
+	Rectangle bounds;
+	double boundWidth, boundHeight, spaceWidth, lineHeight;
+
+	public WordWrap(File file, Rectangle bounds) {
+		pages = new ArrayList<String>();
+		this.file = file;
+		this.bounds = bounds;
+		boundWidth = bounds.getWidth();
+		boundHeight = bounds.getHeight();
+		spaceWidth = new Text(" ").getBoundsInLocal().getWidth();
+		lineHeight = new Text("").getBoundsInLocal().getHeight();
+	}
+
+	public ArrayList<String> readFile() throws IOException {
+		double spaceLeft = boundWidth;
+		Text tempPage = new Text("");
+		String page = "";
+		LineNumberReader r = new LineNumberReader(new FileReader(file));
+		String paragraph, word;
+		while ((paragraph = r.readLine()) != null) {
+			Scanner sc = new Scanner(paragraph);
+			while (sc.hasNext()) {
+				word = sc.next();
+				double wordWidth = new Text(word).getBoundsInLocal().getWidth();
+				double textWithNewLine = tempPage.getBoundsInLocal().getHeight() + lineHeight;
+				if (textWithNewLine > boundHeight) {
+					pages.add(page);
+					page = "";
+				}
+				if (wordWidth + spaceWidth > spaceLeft) {
+					if(!(textWithNewLine > boundHeight))
+						page += "\n";
+					spaceLeft = boundWidth - wordWidth;
+				} else {
+					spaceLeft = spaceLeft - (wordWidth + spaceWidth);
+				}
+				page += word + " ";
+				tempPage.setText(page);
 			}
-			r.close();
-		} catch (IOException e) {
-				e.printStackTrace();
+			if (!((tempPage.getBoundsInLocal().getHeight() + lineHeight) > boundHeight)) {
+				page += "\n";
+				tempPage.setText(page);
+				spaceLeft = boundWidth;
+			}
+			sc.close();
 		}
-    	return pages;
-    }
-    @Override
-    public void start(final Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        final Rectangle r = new Rectangle(400,500);
-        final StackPane root = new StackPane();
-        FlowPane flow = new FlowPane();
-        flow.setPadding(new Insets(5, 0, 5, 0));
-        flow.setHgap(4);
-        r.setFill(Color.WHITE);
-        r.setStroke(Color.BLACK);
-        ArrayList<Text> pages = readFile(r);
-        	flow.getChildren().addAll(pages);
-        root.getChildren().add(flow);
-        
-        primaryStage.setScene(new Scene(root, 900, 600));
-        primaryStage.show();
-        
-    }
+		pages.add(page);
+		r.close();
+		return pages;
+	}
 }
