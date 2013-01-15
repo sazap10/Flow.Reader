d96e5459aede3805d316d97bf10c553d675d0dc2
jim <jim@jim-Ferrari-One-200>
2013-01-15 15:22:59 +0000

    commented out zoom level stuff so everything runs

--- FlowReader/src/flowreader/view/RibbonView.java
+++ FlowReader/src/flowreader/view/RibbonView.java
@@ -43,6 +43,7 @@ public class RibbonView extends Group {
     int minScale = 0;
     int curScale = 81;
     int opaqueScale = 15;
+    int zoomLevels = 0;
     StackPane stackPane;
     StackPane pagesPane;
     StackPane wordCloudPane;
@@ -57,6 +58,7 @@ public class RibbonView extends Group {
     public RibbonView(StackPane stackPane) {
         this.pages = new ArrayList<>();
         this.wordClouds = new ArrayList<>();
+        this.cloudLevels = new HashMap<Integer, ArrayList<WordCloudView>>();
         this.stackPane = stackPane;
     }
 
@@ -71,25 +73,44 @@ public class RibbonView extends Group {
         cloudLevels.put(0, wordClouds);
         while (pageCount >= 1){
             pageCount /= 2;  //half pages
-            ArrayList<WordCloudView> currClouds = mergeClouds(cloudLevels.get(levelCount));
+            System.out.println("Pagecount:" + pageCount);
+            ArrayList<WordCloudView> currClouds = mergeClouds(cloudLevels.get(levelCount), levelCount);
+            System.out.println("done merge!");
             levelCount++;
             cloudLevels.put(levelCount, currClouds);
            
         }
+        this.zoomLevels = levelCount + 1;
+    }
+    
+    //sets the scale needed for the correct level of precision and other stuff
+    public void createZoomTable(){
+        //first, find the final zoom level
+        double finalPercentage = 100 / Math.pow(2, zoomLevels);
+        System.out.println("zoom levels:" + zoomLevels);
+        System.out.println(" final percent: " + finalPercentage);
+        
     }
     
     
     //merges all clouds from one zoom level and outputs half the amount of clouds
     //IMPORTANT: only works with an even number of clouds, final page is missed out at this point,
     //need to improve later
-    public ArrayList<WordCloudView> mergeClouds(ArrayList<WordCloudView> input){
+    public ArrayList<WordCloudView> mergeClouds(ArrayList<WordCloudView> input, int level){
+        System.out.println("calling mergeclouds");
         WordCloudView b = null;
         Boolean haveb = false;
+        double x = input.get(0).getX();
+        double y = 0;
+        double scale = Math.pow(2, level + 1);
         ArrayList<WordCloudView> clouds = new ArrayList<WordCloudView>();
         for (WordCloudView i: input){
             if (haveb && (b!= null)){
-                WordCloudView newCloud = new WordCloudView(i,b);
+                System.out.println("about to call merge cloud constructor");
+                WordCloudView newCloud = new WordCloudView(i,b, new Rectangle(x, y, pageWidth * scale, (pageHeight / 3) * scale));
+                System.out.println("made a cloud!");
                 clouds.add(newCloud);
+                x += pageWidth * scale;
                 haveb = false;
             }
             else{
@@ -110,7 +131,7 @@ public class RibbonView extends Group {
         pagesPane = new StackPane();
         wordCloudPane = new StackPane();
         pagesGroup = new Group();
-        wordCloudGroup = new Group();
+        wordCloudGroup = new Group();      
         while (i < pagesContent.size()) {
 
             WordCloudView wordCloud = new WordCloudView(new Rectangle(x, y, pageWidth, pageHeight/3));
@@ -132,20 +153,27 @@ public class RibbonView extends Group {
             this.getChildren().add(pagesGroup);
             this.getChildren().add(wordCloudGroup);
 
+            
+        System.out.println("now doing cloud zoom levels");
+        //setCloudZoomLevels();
+        //createZoomTable();
+        System.out.println("some");
         //set up zoom levels
         for (int j = 0; j <= maxScale; j++) {
             array[j] = Math.pow(1.05, j - 81);
-            System.out.println("array["+j+"]: "+array[j]);
+            //System.out.println("array["+j+"]: "+array[j]);
         }
+       
         System.out.println("screen properties:"+
                 "\nmax X: "+screenBounds.getMaxX()+
                 "\nmax Y: "+screenBounds.getMaxY());
-        
+       
+       
+                
         this.defineRibbonEvents();
         this.setRibbonEvents(true);
         
         //finally, set the zoom levels for clouds
-        setCloudZoomLevels();
         
     }
 
