53e85831a7e671fa8942203a7dd553a1c5af059a
jim <jim@jim-Ferrari-One-200>
2013-01-31 23:47:53 +0000

    made function alterations to pass frequency data to wordCloud constructor

--- FlowReader/src/flowreader/model/Document.java
+++ FlowReader/src/flowreader/model/Document.java
@@ -4,6 +4,7 @@
  */
 package flowreader.model;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -11,17 +12,20 @@ import java.util.ArrayList;
  */
 public class Document {
     ArrayList<Page> pages;
+    HashMap<String, Integer> documentOccurrences; //wordOccurrences in the document
     ArrayList<ArrayList<WordCloud>> wordClouds;
     int numOfPages;
     int numOfCloudLevels;
+    int wordCount;
     
-    
-    public Document(ArrayList<Page> pages, ArrayList<ArrayList<WordCloud>> wordClouds){
+    public Document(ArrayList<Page> pages, ArrayList<ArrayList<WordCloud>> wordClouds, HashMap<String, Integer> documentOccurrences, int wordCount){
         this.pages = pages;
         this.wordClouds = wordClouds;
         this.numOfPages = pages.size();
         this.numOfCloudLevels = wordClouds.size();
-        
+        this.documentOccurrences = documentOccurrences;
+        this.wordCount = wordCount;
+        setWordCloudValues();
     }
     
     public ArrayList<Page> getPages(){
@@ -47,6 +51,27 @@ public class Document {
     
     public int getNumOfCloudLevels(){
         return this.numOfCloudLevels;
+    }
+    
+    //gets the word count for each page and adds it to the list
+   
+    public void setWordCloudValues(){
+        int level = 1;
+        int currPage;
+        int wordCount;
+        for(ArrayList<WordCloud> cloudList : this.wordClouds){
+            currPage = 1;
+            for(WordCloud cloud : cloudList){
+                //need to sum the wordCounts from the next (level) pages
+                wordCount = 0;
+                for(int i = 1; i <= level; i++){
+                    wordCount += this.pages.get(currPage - 1).getWordCount();                 
+                }
+                cloud.setWordValues(documentOccurrences, wordCount, this.wordCount);
+            }
+            level++;
+            
+        }
     }
     
 }
