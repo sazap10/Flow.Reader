9c6667884529510cc7547f2db74441461b9ca0b3
jim <jim@jim-Ferrari-One-200>
2013-02-06 19:00:37 +0000

    changed structure of document to store page images

--- FlowReader/src/flowreader/model/Document.java
+++ FlowReader/src/flowreader/model/Document.java
@@ -5,13 +5,62 @@
 package flowreader.model;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.awt.image.BufferedImage;
+import javafx.scene.image.Image;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
 /**
  *
  * @author jim
  */
 public class Document {
-    ArrayList<Page> pages;
+    ArrayList<BufferedImage> inputImages;
+    ArrayList<Image> pages;
+    int numOfPages;
+    public Document(ArrayList<BufferedImage> pages){
+        
+        this.pages = new ArrayList();
+        this.inputImages = pages;
+        this.numOfPages = this.inputImages.size();
+    }
+    
+    public Image getPage(int index){
+        return this.pages.get(index);
+    }
+        
+    
+    public void resizePages(int pageHeight, int pageWidth) throws IOException{
+        for(BufferedImage image: inputImages){
+            pages.add(createImage(image, pageHeight, pageWidth));
+        }
+        
+    }
+    public static Image createImage(java.awt.Image image, int height, int width) throws IOException {
+    if (!(image instanceof RenderedImage)) {
+      BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),
+              image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+      Graphics g = bufferedImage.createGraphics();
+      g.drawImage(image, 0, 0, null);
+      g.dispose();
+      image = bufferedImage;
+    }
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ImageIO.write((RenderedImage) image, "png", out);
+    out.flush();
+    ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+    return new javafx.scene.image.Image(in, width, height, false, false );
+  }
+       
+    
+    
+    
+    
+ /*   ArrayList<Page> pages;
     HashMap<String, Integer> documentOccurrences; //wordOccurrences in the document
     ArrayList<ArrayList<WordCloud>> wordClouds;
     int numOfPages;
@@ -44,11 +93,11 @@ public class Document {
     public ArrayList<WordCloud> getCloudLevel(int level){
         return wordClouds.get(level -1 );
     }
-    
+    */
     public int getNumOfPages(){
         return this.numOfPages;
     }
-    
+    /*
     public int getNumOfCloudLevels(){
         return this.numOfCloudLevels;
     }
@@ -73,5 +122,5 @@ public class Document {
             
         }
     }
-    
+    */
 }
