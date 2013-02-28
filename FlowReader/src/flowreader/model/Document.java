ba59f9344eb733345b1b6ed470d46dd5604e91f3
jim <jim@jim-Ferrari-One-200>
2013-02-28 11:41:17 +0000

    added several functions to allow new implementation in textfilereader

--- FlowReader/src/flowreader/model/Document.java
+++ FlowReader/src/flowreader/model/Document.java
@@ -5,8 +5,9 @@
 package flowreader.model;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.awt.image.BufferedImage;
+import javafx.scene.image.WritableImage;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.awt.image.RenderedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -19,18 +20,19 @@ import java.awt.Graphics;
  * @author jim
  */
 public class Document {
-    ArrayList<BufferedImage> inputImages;
-    ArrayList<Image> pages;
+    ArrayList<WritableImage> inputImages;
+    ArrayList<ImageView> pages;
     ArrayList<Integer> pageWordCounts;
     HashMap<String, Integer> documentOccurrences; //wordOccurrences in the document
     ArrayList<ArrayList<WordCloud>> wordClouds;
     int numOfPages;
     int numOfCloudLevels;
     int wordCount;
+    int pageWidth;
+    int pageHeight;
     
-     public Document(ArrayList<BufferedImage> pages, ArrayList<Integer> pageWordCounts,  ArrayList<ArrayList<WordCloud>> wordClouds, HashMap<String, Integer> documentOccurrences, int wordCount){
+  /*   public Document(ArrayList<WritableImage> pages, ArrayList<Integer> pageWordCounts,  ArrayList<ArrayList<WordCloud>> wordClouds, HashMap<String, Integer> documentOccurrences, int wordCount){
         this.pages = new ArrayList();
-        this.inputImages = pages;
         this.pageWordCounts = pageWordCounts;
         this.wordClouds = wordClouds;
         this.numOfPages = pages.size();
@@ -38,26 +40,61 @@ public class Document {
         this.documentOccurrences = documentOccurrences;
         this.wordCount = wordCount;
         setWordCloudValues();
+        System.out.println("no exception here");
+        resizePages(700*2, 500*2);
     }
-    public Document(ArrayList<BufferedImage> pages){
+    
+    public Document(ArrayList<WritableImage> pages){
         
         this.pages = new ArrayList();
         this.inputImages = pages;
         this.numOfPages = this.inputImages.size();
     }
+    */
     
-    public Image getPage(int index){
+    public Document(int pageHeight, int pageWidth){
+        this.pages = new ArrayList();
+        this.pageWidth = pageWidth;
+        this.pageHeight = pageHeight;
+    }
+    public void addImage(WritableImage image){
+           ImageView newPage = new ImageView(image);
+           newPage.setFitWidth(this.pageWidth);
+           newPage.setFitHeight(this.pageHeight);
+           pages.add(newPage);
+        
+    }
+    
+    public void addPageCounts(ArrayList<Integer> pageWordCounts){
+        this.pageWordCounts = pageWordCounts;
+        
+    }
+    
+    public void addWordClouds(ArrayList<ArrayList<WordCloud>> clouds, HashMap<String, Integer> documentOccurrences, int wordCount){
+        this.wordClouds = clouds;
+        this.numOfPages = pages.size();
+        this.numOfCloudLevels = wordClouds.size();
+        this.documentOccurrences = documentOccurrences;
+        this.wordCount = wordCount;
+        setWordCloudValues();
+    }
+    
+    public ImageView getPage(int index){
         return this.pages.get(index);
     }
         
     
-    public void resizePages(int pageHeight, int pageWidth) throws IOException{
-        for(BufferedImage image: inputImages){
-            pages.add(createImage(image, pageHeight, pageWidth));
+    public void resizePages(int pageHeight, int pageWidth){
+        for(WritableImage image: inputImages){
+           ImageView newPage = new ImageView(image);
+           newPage.setFitWidth(pageWidth);
+           newPage.setFitHeight(pageHeight);
+           pages.add(newPage);
+            
         }
         
     }
-    public static Image createImage(java.awt.Image image, int height, int width) throws IOException {
+ /*   public static Image createImage(java.awt.Image image, int height, int width) throws IOException {
     if (!(image instanceof RenderedImage)) {
       BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),
               image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
@@ -73,7 +110,7 @@ public class Document {
     return new javafx.scene.image.Image(in, width, height, false, false );
   }
        
-    
+ */   
     
     
 /*
